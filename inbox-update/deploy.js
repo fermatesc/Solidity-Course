ca54055ba0da8942ab356fa60068318735f164d5
Fernando Mateo <fmateoe@ext.laligatech.com>
2022-07-02 23:37:05 +0200

    first commit

---
+++ inbox-update/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'slab amount muscle brisk nation iron gift army dice width orient clog',
+  'https://rinkeby.infura.io/v3/27f999025f0c40d89ca2994ddb29831c'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
---
+++ inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider('slab amount muscle brisk nation iron gift army dice width orient clog',
+                                      'https://rinkeby.infura.io/v3/27f999025f0c40d89ca2994ddb29831c');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Atempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deploy to:', result.options.address);
+
+  provider.engine.stop();
+};
+
+deploy();
+
+//0x7ed3A6784aa33be9abEcD242552cb2cb4405Fd1f
