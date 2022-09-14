61034df48d4d4854ffe724963ec55c98f94cde82
Fernando Mateo <fmateoe@ext.laligatech.com>
2022-09-14 15:49:18 +0200

    modify lottery proyect

---
+++ lottery-boilerplate/lottery/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'slab amount muscle brisk nation iron gift army dice width orient clog',
+  // remember to change this to your own phrase!
+  'https://rinkeby.infura.io/v3/27f999025f0c40d89ca2994ddb29831c'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
